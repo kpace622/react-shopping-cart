6159bfe976684aa845c6ce14f19f821d9d6696cb
Kyle Pace <kpace2296@gmail.com>
2020-01-29 20:50:27 -0600

    error

--- src/App.js
+++ src/App.js
@@ -7,35 +7,51 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+import ProuctContext from '../src/contexts/ProductContext';
+import CartContext from '../src/contexts/CartContext';
+
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
+		setCart({...cart, cart: item})
 		// add the given item to the cart
 	};
 
+
 	return (
-		<div className="App">
-			<Navigation cart={cart} />
+		<ProuctContext.Provider value={{ products, addItem }}>
+			<CartContext.Provider value={cart}>
+				<div className="App">
+					<Navigation cart={cart} />
 
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
+					{/* Routes */}
+					{/* <Route
+						exact
+						path="/"
+						render={() => (
+							<Products
+								products={products}
+								addItem={addItem}
+							/>
+						)}
+					/> */}
+
+					<Route exact path='/' component={Products} />
+
+					{/*
+					 <Route></Route>
+						path="/cart"
+						render={() => <ShoppingCart cart={cart} />}
 					/>
-				)}
-			/>
+					*/}
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
-		</div>
+					<Route path='/cart' component={cart} />
+
+				</div>
+			</CartContext.Provider>
+		</ProuctContext.Provider>
 	);
 }
 
--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 // Components
 import Item from './ShoppingCartItem';
 
+import CartContext from '../contexts/CartContext';
+
 const ShoppingCart = props => {
+
+	
 	const getCartTotal = () => {
 		return props.cart.reduce((acc, value) => {
 			return acc + value.price;
@@ -15,7 +19,6 @@ const ShoppingCart = props => {
 			{props.cart.map(item => (
 				<Item key={item.id} {...item} />
 			))}
-
 			<div className="shopping-cart__checkout">
 				<p>Total: ${getCartTotal()}</p>
 				<button>Checkout</button>
